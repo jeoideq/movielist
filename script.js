678bb9b92462b1717e7c586cff772516f76c599b
jeoideq <hanssonj20@gmail.com>
2026-02-22 13:09:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function get_value()
+{
+    let name=document.getElementById("name").value;
+    let person=document.getElementById("person").value;
+    let year=document.getElementById("year").value;
+    
+
+
+}
+ 
