aebcb4877bff9cb61dbf5b4f426caf78e33911a1
jeoideq <hanssonj20@gmail.com>
2026-02-26 17:55:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-function get_value()
+function get_value(event)
 {
+   
     let name=document.getElementById("name").value;
     let person=document.getElementById("person").value;
     let year=document.getElementById("year").value;
-    
+    const row=document.createElement("tr");
+    const col1=document.createElement("td");
+    const col2=document.createElement("td");
+    const col3=document.createElement("td");
+    col1.textContent=name;
+    col2.textContent=person;
+    col3.textContent=year;
+    row.appendChild(col1);
+    row.appendChild(col2);
+    row.appendChild(col3);
+    let tablebody=document.getElementById("tablebody");
+    tablebody.appendChild(row);
 
-
+    document.getElementById("name").value="";
+    document.getElementById("person").value="";
+    document.getElementById("year").value="";
 }
- 
+
+
